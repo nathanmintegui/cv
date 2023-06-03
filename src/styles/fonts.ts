ef3f30f43b67ac66e63287d39e2e2b1d1c567425
nathanmintegui <nathanberger.rs@hotmail.com>
2023-06-02 23:11:33 -0300

    feat(fonts): add fonts config

---
+++ src/styles/fonts.ts
@@ -0,0 +1,9 @@
+import { Rubik, Space_Mono } from "next/font/google";
+
+const rubik700 = Rubik({ weight: "700", subsets: ["latin"] });
+const rubik800 = Rubik({ weight: "800", subsets: ["latin"] });
+
+const spaceMono400 = Space_Mono({ weight: "400", subsets: ["latin"] });
+const spaceMono700 = Space_Mono({ weight: "700", subsets: ["latin"] });
+
+export { rubik700, rubik800, spaceMono400, spaceMono700 };
